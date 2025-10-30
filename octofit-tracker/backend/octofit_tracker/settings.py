997aea727dab38965d2fbdf16ee55d5e74332d20
Ravi Dixit <rvdxt03@gmail.com>
2025-10-30 20:50:01 +0000

    Update settings.py to support dynamic Codespace URL in ALLOWED_HOSTS

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-zv+b2zp*2&o30ve+^9=utg8%d=68vnc*!w5=573j=$5o(8m$k=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{CODESPACE_NAME}-8000.app.github.dev" if CODESPACE_NAME else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
 
 
 # Application definition
